c9f8a101503fb7a753c2d6cb484a429eb4749f3c
Abzal <serikbay.a04@gmail.com>
2024-11-06 15:44:25 +0500

    added item info page

--- app/src/main/java/com/example/mini_shop/ItemsAdapter.kt
+++ app/src/main/java/com/example/mini_shop/ItemsAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.mini_shop
 
 import android.content.Context
+import android.content.Intent
 import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView
         val title: TextView = view.findViewById(R.id.item_list_title)
         val desc: TextView = view.findViewById(R.id.item_list_desc)
         val price: TextView = view.findViewById(R.id.item_list_price)
+        val btn: Button = view.findViewById(R.id.item_list_button)
     }
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -38,5 +41,11 @@ import androidx.recyclerview.widget.RecyclerView
              context.packageName
          )
          holder.image.setImageResource(imageId)
+         holder.btn.setOnClickListener{
+             val intent = Intent(context, ItemActivity::class.java)
+             intent.putExtra("itemTitle", items[position].title)
+             intent.putExtra("itemText", items[position].text)
+             context.startActivity(intent)
+         }
      }
  }
