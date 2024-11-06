17b32c57c599c6acd2fc856ae38420c5e97cefa5
Abzal <serikbay.a04@gmail.com>
2024-11-06 16:15:58 +0500

    not finished payment

--- app/src/main/java/com/example/mini_shop/ItemActivity.kt
+++ app/src/main/java/com/example/mini_shop/ItemActivity.kt
@@ -1,18 +1,67 @@
 package com.example.mini_shop
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.razorpay.Checkout
+import com.razorpay.ExternalWalletListener
+import com.razorpay.PaymentData
+import com.razorpay.PaymentResultWithDataListener
+import org.json.JSONObject
 
-class ItemActivity : AppCompatActivity() {
+class ItemActivity : AppCompatActivity(), PaymentResultWithDataListener, ExternalWalletListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
 
+        Checkout.preload(applicationContext)
+        val co = Checkout()
+        co.setKeyID("rzp_test_EgmQoGwAT33eJi")
+
         val title: TextView = findViewById(R.id.item_list_title_one)
         val text: TextView = findViewById(R.id.item_list_text)
+        val btn: Button = findViewById(R.id.button_buy)
 
         title.text = intent.getStringExtra("itemTitle")
         text.text = intent.getStringExtra("itemText")
+
+        btn.setOnClickListener{
+            startPayment()
+        }
+    }
+    private fun startPayment() {
+        val activity: Activity = this
+        val co = Checkout()
+
+        try {
+            val options = JSONObject()
+            options.put("name","Razorpay Corp")
+            options.put("description","Demoing Charges")
+            options.put("image","http://example.com/image/rzp.jpg")
+            options.put("theme.color", "#3399cc");
+            options.put("currency","INR");
+            options.put("order_id", "order_DBJOWzybf0sJbb");
+            options.put("amount",10000)
+
+            co.open(activity,options)
+        }catch (e: Exception){
+            Toast.makeText(activity,"Error in payment: "+ e.message, Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
+    }
+
+    override fun onPaymentSuccess(p0: String?, p1: PaymentData?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPaymentError(p0: Int, p1: String?, p2: PaymentData?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onExternalWalletSelected(p0: String?, p1: PaymentData?) {
+        TODO("Not yet implemented")
     }
 }
